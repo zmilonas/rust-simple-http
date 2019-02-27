f9c83fa859546597e15deb236ee456e097d03f1b
Zachary Milonas <zachary@milonas.pl>
2019-02-27 22:36:19 +0100

    More stringy approach

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
   let args: Vec<String> = std::env::args().collect();
-  let response = match args.len() {
-    2 => args[1].trim(),
-    _ => "noname"
+  let response: String = match args.len() {
+    2 => args[1].to_string(),
+    _ => String::from("noname")
   };
 
   println!("Hello {}", response);
 }
+
